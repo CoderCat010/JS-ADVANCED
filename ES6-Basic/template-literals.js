dbd31bcf5b8d89324916f2db3572d5d1dd6aac12
Ummay Nusra <nusraumme@gmail.com>
2026-04-28 09:12:08 +0600

    added practice-10

--- ES6-Basic/template-literals.js
+++ ES6-Basic/template-literals.js
@@ -1,8 +1,15 @@
-const a = 5;
-const b = 5;
-const result = `The sum of ${a} and ${b} is ---> ${a + b}`;
-console.log(result);
+// const a = 5;
+// const b = 5;
+// const result = `The sum of ${a} and ${b} is ---> ${a + b}`;
+// console.log(result);
 
-// new line
-const text = `jnjcdnjcnjdcnjdcjdcjdcnjdcnjdcnjdcnjdncjdncjdcn`
+// // new line
+// const text1 = 'jnjcdnjcnjdcnjdcj \n'+ 
+// 'dcjdcnjdcnjdcnjdcnjdncjdncjdcn';
+// console.log(text1);
+
+const text2 = `jnjcdnjcnjdcnjdcjdcjdcnjdcnjdcnjdcnjdnc
+jdncjdcn`;
+console.log(text2);
+
 
