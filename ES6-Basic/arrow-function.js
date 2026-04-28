b19b7a2b7996f9cbf52ddbf0b7a0befd26ab7c82
Ummay Nusra <nusraumme@gmail.com>
2026-04-28 11:51:30 +0600

    added practice-13

--- ES6-Basic/arrow-function.js
+++ ES6-Basic/arrow-function.js
@@ -1,6 +1,11 @@
 // const sum = (a, b) =>  a + b;
 // console.log(sum(2, 3));
 
-const getArrElm = (person) => person.age;
-console.log(getArrElm({name: 'anu', age: 30}));
+// object
+// const getObjElm = (person) => person.age;
+// console.log(getObjElm({name: 'anu', age: 30}));
+
+// array
+const getArrElm = (element) => element[3];
+console.log(getArrElm([1, 2, 3, 4]));
 
