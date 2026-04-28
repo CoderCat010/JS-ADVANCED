e6e700738e906665d64eeb50adb8d3d52e6f0157
Ummay Nusra <nusraumme@gmail.com>
2026-04-28 12:03:53 +0600

    added practice-14

--- ES6-Basic/arrow-function.js
+++ ES6-Basic/arrow-function.js
@@ -1,11 +1,24 @@
+// implicit return ----> এক লাইনে রিটার্ন - {} এবং return লিখতে হয় না
 // const sum = (a, b) =>  a + b;
 // console.log(sum(2, 3));
+
+
+// explicit return ----> একাধিক লাইনের জন্য {} এবং return লাগবে
+// const sum = (num1, num2, num3, num4) => {
+//     const add = num1 + num2;
+//     const multiply = num3 * num4;
+
+//     return `result: ${add}, ${multiply}`;
+// }
+// console.log(sum(1, 2, 3, 4));
+
+
 
 // object
 // const getObjElm = (person) => person.age;
 // console.log(getObjElm({name: 'anu', age: 30}));
 
 // array
-const getArrElm = (element) => element[3];
-console.log(getArrElm([1, 2, 3, 4]));
+// const getArrElm = (element) => element[3];
+// console.log(getArrElm([1, 2, 3, 4]));
 
