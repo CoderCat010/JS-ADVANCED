522f19b04eb8c149001c04404b6b8fd27ab687f4
Ummay Nusra <nusraumme@gmail.com>
2026-04-29 07:10:16 +0600

    added practice-16

--- ES6-Basic/destructuring.js
+++ ES6-Basic/destructuring.js
@@ -4,5 +4,11 @@ const person = {
     address: 'dhaka',
     occupation: 'student'
 }
-const {name, address, occupation} = person;
-console.log(name);
+// const {name, address, occupation} = person;
+// console.log(name);
+
+// we can rename the properties name
+const {name: userName, address, occupation} = person;
+// console.log(name);
+console.log(userName);
+
