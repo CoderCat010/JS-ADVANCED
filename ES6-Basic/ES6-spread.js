131811c31f7d338a3296020dc6bb6ef2c1c5a3b8
Ummay Nusra <nusraumme@gmail.com>
2026-04-29 19:22:34 +0600

    added practice-20

--- ES6-Basic/ES6-spread.js
+++ ES6-Basic/ES6-spread.js
@@ -1,5 +1,8 @@
-const arr = [1, 2, 3, 4, 5];
-const arry = [6, 7, 8, 9, 0];
+// const arr = [1, 2, 3, 4, 5];
+// const arry = [6, 7, 8, 9, 0];
 // const add = [...arr, ...arry];
-const add = [...arr, 99,'addffd', ...arry];
-console.log(add);
+// const add = [...arr, 99,'addffd', ...arry];
+// console.log(add);
+
+const fruits = ['আম', 'কাঁঠাল', 'লিচু'];
+console.log(...fruits);
