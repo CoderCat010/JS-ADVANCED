2da8ff3ca5275ab416a32ec5191bc91e096353d6
Ummay Nusra <nusraumme@gmail.com>
2026-04-29 07:07:39 +0600

    added practice-15

---
+++ ES6-Basic/destructuring.js
@@ -0,0 +1,8 @@
+//-----> object destructuring
+const person = {
+    name: 'billi',
+    address: 'dhaka',
+    occupation: 'student'
+}
+const {name, address, occupation} = person;
+console.log(name);
