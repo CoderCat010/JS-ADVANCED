0bbe8abe6ac9b45a3a4578fed71988c99ae7d777
Ummay Nusra <nusraumme@gmail.com>
2026-05-02 11:02:57 +0600

    added practice-3

--- ES6-Basic-2/ES6-arrayChaining-Methods.js
+++ ES6-Basic-2/ES6-arrayChaining-Methods.js
@@ -1,12 +1,19 @@
 //-----> map method
 const arrElm = [1, 2, 3, 4];
 
-// regular function
+//---> regular function
 // const a = arrElm.map(function(elm){
 //     return elm * elm;
 // })
 // console.log(a);
 
-// arrow function
-const a = arrElm.map(elm => elm * elm)
+//---> arrow function
+// const a = arrElm.map(elm => elm * elm)
+// console.log(a);
+
+//---> arrow function + curly bracket
+const a = arrElm.map(elm => {
+    return elm * elm;
+})
+
 console.log(a);
