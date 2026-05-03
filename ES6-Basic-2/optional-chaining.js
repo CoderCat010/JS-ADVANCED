0d8cff45565c17102e55ddf5755f22cc55672013
Ummay Nusra <nusraumme@gmail.com>
2026-05-03 15:11:24 +0600

    added practice-5

--- ES6-Basic-2/optional-chaining.js
+++ ES6-Basic-2/optional-chaining.js
@@ -37,16 +37,20 @@
 // console.log(user?.address?.newAddress?.newAddress2);
 
 
-let user = { 
-    name: "Rahim", 
-    address: { 
-        city: "Dhaka",
-        newAddress:{
-            newCity: 'madaripur', 
-            newAddress2:{
-                newCity: 'dhaka'
-            }
-        }
-    } 
-};
-console.log(user?.address?.newAddress?.newAddress2?.['newCity']);
+// let user = { 
+//     name: "Rahim", 
+//     address: { 
+//         city: "Dhaka",
+//         newAddress:{
+//             newCity: 'madaripur', 
+//             newAddress2:{
+//                 newCity: 'dhaka'
+//             }
+//         }
+//     } 
+// };
+// console.log(user?.address?.newAddress?.newAddress2?.['newCity']);
+
+
+const arrElm = [3, 4, 5, 77, 22, 5, 2];
+console.log(arrElm?.[3]);
