f46787c4a2144167bb7c474e239038369a19347a
Ummay Nusra <nusraumme@gmail.com>
2026-05-11 11:30:17 +0600

    added practice-1

---
+++ ES6-Basic-2/basic-problem-solvings.js
@@ -0,0 +1,11 @@
+const polapain = [
+    {name: 'abul', job: 'sarkari', salary: 990000},
+    {name: 'kabul', job: 'besarkari', salary: 30000},
+    {name: 'babul', job: 'sarkari', salary: 540000},
+    {name: 'tabul', job: 'sarkari', salary: 340000},
+    {name: 'fabul', job: 'besarkari', salary: 560000},
+    {name: 'mokbul', job: 'besarkari', salary: 130000},
+    {name: 'kuddus', job: 'sarkari', salary: 10000},
+    {name: 'kamrul', job: 'besarkari', salary: 740000}
+];
+
