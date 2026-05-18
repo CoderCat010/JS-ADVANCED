037d9ef24bf5e2d204ba9d7695876fb5390695de
Ummay Nusra <nusraumme@gmail.com>
2026-05-18 11:17:26 +0600

    added practice-8

--- ES6-Advanced-1/load-allPost.js
+++ ES6-Advanced-1/load-allPost.js
@@ -2,5 +2,10 @@ const loadPost = () => {
     const url = 'https://jsonplaceholder.typicode.com/posts/';
     fetch(url)
     .then((res) => res.json())
-    .then((json) => console.log(json))
+    .then((json) => {
+        json.forEach((post) => {
+            console.log(post.title);
+        });
+    });
 }
+// loadPost()
