0b285fd6e2b5f1611a98b7fd1f6edea43ae6d05f
Ummay Nusra <nusraumme@gmail.com>
2026-05-18 11:02:11 +0600

    added practice-6

--- ES6-Advanced-1/json.js
+++ ES6-Advanced-1/json.js
@@ -20,12 +20,11 @@ const obj = {
 
 
 
-const loadData = () =>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-}
-console.log(loadData);
+// const loadData = () =>{
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// }
 
 
 
