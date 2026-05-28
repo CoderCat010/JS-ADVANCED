04fdf8a8d9256c62ef24b53b78474f70354c71b5
Ummay Nusra <nusraumme@gmail.com>
2026-05-28 22:34:14 +0600

    added practice-4

--- ES6-Recap/Arrow-Function.js
+++ ES6-Recap/Arrow-Function.js
@@ -4,7 +4,12 @@
 // const addNum = () => 2 + 3;
 // console.log(addNum());
 
-const addNum = (n) => n + 3;
+// const addNum = (n) => n + 3;
+// console.log(addNum(2));
+
+const addNum = (n) => {
+    return n + 3;
+} 
 console.log(addNum(2));
 
 
