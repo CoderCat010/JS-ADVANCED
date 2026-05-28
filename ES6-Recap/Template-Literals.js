7219715e35d3186f1885a70863d45f8516728fcd
Ummay Nusra <nusraumme@gmail.com>
2026-05-28 23:28:54 +0600

    added new file

--- ES6-Recap/Template-Literals.js
+++ ES6-Recap/Template-Literals.js
@@ -1,6 +1,6 @@
-var name = "Rahim";
-var age = 25;
-var city = "Dhaka";
+// var name = "Rahim";
+// var age = 25;
+// var city = "Dhaka";
 
-var allInOne = `my name is ${name}, im ${age} years old, i live in ${city}`;
-console.log(allInOne);
+// var allInOne = `my name is ${name}, im ${age} years old, i live in ${city}`;
+// console.log(allInOne);
