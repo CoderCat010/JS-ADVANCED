2b201396319c318639e6567512c984dc4c6a79bc
Ummay Nusra <nusraumme@gmail.com>
2026-05-30 20:42:06 +0600

    added practice-6

--- ES6-Recap/Rest-Spread-Operators.js
+++ ES6-Recap/Rest-Spread-Operators.js
@@ -3,20 +3,22 @@
 // }
 // console.log(addNum(2, 3, 4, 5));
 
-// const person = { 
-//      name: "Karim",
-//      age: 25, 
-//      city: "Dhaka",
-//      job: "Dev", 
-//      hobby: "Gaming" 
-// };
+const person = { 
+     name: "Karim",
+     age: 25, 
+     city: "Dhaka",
+     job: "Dev", 
+     hobby: "Gaming" 
+};
 
 // const {name, age, ...rest} = person;
 
 // console.log(name);
 // console.log(rest);
+console.log(...person); // cannot spread plain object
 
-const nums = [1, 2, 3, 4, 5];
-console.log(...nums);
+
+// const nums = [1, 2, 3, 4, 5];
+// console.log(...nums);
 
 
