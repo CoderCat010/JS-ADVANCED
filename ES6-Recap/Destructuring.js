a895ad5d45a1a904d3fcaa609b891e0d3a221126
Ummay Nusra <nusraumme@gmail.com>
2026-05-30 16:10:25 +0600

    added practice-3

--- ES6-Recap/Destructuring.js
+++ ES6-Recap/Destructuring.js
@@ -1,9 +1,14 @@
-const car = {
-    model: 'tesla auto',
-    color: 'red',
-    price: 60000
-}
+// const carObj = {
+//     model: 'tesla auto',
+//     color: 'red',
+//     price: 60000
+// }
 
-// console.log(car.model);
-const {model: m, color, price} = car;
-console.log(m, color, price);
+// // console.log(car.model);
+// const {model: m, color, price} = carObj;
+// console.log(m, color, price);
+
+
+const arrElm = [1, 3, 4];
+const [f, g, h] = arrElm;
+console.log(f, g, h);
