05fce26aa5010b92325175f701dcb14cb27f94dd
Ummay Nusra <nusraumme@gmail.com>
2026-05-30 20:24:12 +0600

    added practice-4

--- ES6-Recap/Rest-Spread-Operators.js
+++ ES6-Recap/Rest-Spread-Operators.js
@@ -1,4 +1,20 @@
-function addNum(...nums){
-    return (nums);
-}
-console.log(addNum(2, 3, 4, 5));
+// function addNum(...nums){
+//     return (nums);
+// }
+// console.log(addNum(2, 3, 4, 5));
+
+const person = { 
+     name: "Karim",
+     age: 25, 
+     city: "Dhaka",
+     job: "Dev", 
+     hobby: "Gaming" 
+};
+
+const {name, age, ...rest} = person;
+
+console.log(name);
+console.log(rest);
+
+
+
